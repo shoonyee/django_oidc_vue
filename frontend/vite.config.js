872fecf2f288b1528c38681676840cbbeda5dae7
Shun Han <shunhan@umich.edu>
2025-08-13 14:23:46 -0400

    add dockerfiles

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,11 +13,11 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8080',
         changeOrigin: true
       },
       '/oidc': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8080',
         changeOrigin: true
       }
     }
